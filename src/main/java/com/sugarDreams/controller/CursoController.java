7e87a8ac4090efac8c665e82324f48eb3fe2a897
IndiraRamirez <145410298+IndiraRamirez@users.noreply.github.com>
2024-07-29 13:53:38 -0600

    avance Indira

--- src/main/java/com/sugarDreams/controller/CursoController.java
+++ src/main/java/com/sugarDreams/controller/CursoController.java
@@ -3,6 +3,9 @@ package com.sugarDreams.controller;
 import com.sugarDreams.domain.Curso;
 import com.sugarDreams.service.CursoService;
 import com.sugarDreams.service.FirebaseStorageService;
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,73 +20,102 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("/curso")
 public class CursoController {
-
-    @Autowired
+     @Autowired
     private CursoService cursoService;
-
+    
     @GetMapping("/reposteria")
-    public String reposteria(Model model, Curso curso) {
-        var lista = cursoService.getCursos(curso);
-        List<Curso> cursosReposteria = lista.stream()
+    public String reposteria(Model model,Curso curso){
+         var lista = cursoService.getCursos(curso);
+         List<Curso> cursosReposteria = lista.stream()
                 .filter(c -> c.getCategoria().equalsIgnoreCase("Reposteria"))
                 .collect(Collectors.toList());
-        model.addAttribute("cursosReposteria", cursosReposteria);
+         
+         model.addAttribute("cursosReposteria", cursosReposteria);
+        
+        
         return "/curso/reposteria";
+        
     }
-
     @GetMapping("/brownie")
-    public String brownie(Model model, Curso curso) {
-        var lista = cursoService.getCursos(curso);
-        List<Curso> cursosBrownie = lista.stream()
+    public String brownie(Model model,Curso curso){
+         var lista = cursoService.getCursos(curso);
+         List<Curso> cursosBrownie = lista.stream()
                 .filter(c -> c.getCategoria().equalsIgnoreCase("Brownie"))
                 .collect(Collectors.toList());
-        model.addAttribute("cursosBrownie", cursosBrownie);
+         
+         model.addAttribute("cursosBrownie", cursosBrownie);
+        
+        
+        
+        
         return "/curso/brownie";
+        
     }
-
     @GetMapping("/queque")
-    public String queque(Model model, Curso curso) {
-        var lista = cursoService.getCursos(curso);
-        List<Curso> cursosQueque = lista.stream()
+    public String queque(Model model,Curso curso){
+         var lista = cursoService.getCursos(curso);
+         List<Curso> cursosQueque = lista.stream()
                 .filter(c -> c.getCategoria().equalsIgnoreCase("Queque"))
                 .collect(Collectors.toList());
-        model.addAttribute("cursosQueque", cursosQueque);
+         
+         model.addAttribute("cursosQueque", cursosQueque);
+        
+        
+        
         return "/curso/queque";
+        
     }
-
     @GetMapping("/quequeSeco")
-    public String quequeSeco(Model model, Curso curso) {
-        var lista = cursoService.getCursos(curso);
-        List<Curso> cursosQuequeSeco = lista.stream()
+    public String quequeSeco(Model model,Curso curso){
+         var lista = cursoService.getCursos(curso);
+         List<Curso> cursosQuequeSeco = lista.stream()
                 .filter(c -> c.getCategoria().equalsIgnoreCase("QuequeSeco"))
                 .collect(Collectors.toList());
-        model.addAttribute("cursosQuequeSeco", cursosQuequeSeco);
+         
+         model.addAttribute("cursosQuequeSeco", cursosQuequeSeco);
+        
+        
+        
         return "/curso/quequeSeco";
-
+        
     }
-
     @PostMapping("/guardar")
-    public String guardar(Curso curso) {
-        cursoService.save(curso);
-        return "redirect:/curso/reposteria";
+    public String guardar(Curso curso){
+        
+            cursoService.save(curso);
+           
+        return "redirect:/curso/confirmacionAgregar";
+        
+        
     }
-
+    
     @GetMapping("/eliminar/{idCurso}")
-    public String eliminar(Curso curso) {
+    public String eliminar(Curso curso){
         cursoService.delete(curso);
-        return "redirect:/curso/reposteria";
+        return "redirect:/curso/confirmacionModificacion";
+        
     }
-
     @GetMapping("/modificar/{idCurso}")
-    public String modificar(Curso curso, Model model) {
+    public String modificar (Curso curso,Model model){
         curso = cursoService.getIdCurso(curso);
         model.addAttribute("curso", curso);
+                
         return "/curso/modificar";
     }
-
     @GetMapping("/confirmacion")
-    public String confirmacion() {
+    public String confirmacion(){
         return "/curso/confirmacion";
+        
     }
+    @GetMapping("/confirmacionAgregar")
+    public String confirmacionAgregar(){
+        return "/curso/confirmacionAgregar";  
+    }
+    
+    @GetMapping("/confirmacionModificacion")
+    public String confirmacionModificacion(){
+        return "/curso/confirmacionModificacion";  
+    }
+    
 
 }
